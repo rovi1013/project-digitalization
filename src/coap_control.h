3112be8e98ed67b11c320e10d29cef58621a3408
Vinc_ent <38655026+rovi1013@users.noreply.github.com>
2025-02-03 15:24:04 +0100

    Added struct for coap message

--- src/coap_control.h
+++ src/coap_control.h
@@ -7,12 +7,24 @@
 #include <net/gcoap.h>
 #include "net/sock/udp.h"
 
-#define MAX_CHAT_IDS 20
+#define URL_LENGTH 35
+#define ADDRESS_LENGTH 45
+#define PORT_LENGTH 5
+#define ENDPOINT_LENGTH 20
+#define MESSAGE_DATA_LENGTH 30
+#define MAX_CHAT_IDS 10
 #define CHAT_ID_LENGTH 12
 #define BOT_TOKEN_LENGTH 50
 
-extern const char telegram_bot_token[BOT_TOKEN_LENGTH];
-extern const char chat_ids[MAX_CHAT_IDS][CHAT_ID_LENGTH];
+typedef struct {
+    char url[URL_LENGTH];
+    char address[ADDRESS_LENGTH];
+    char port[PORT_LENGTH];
+    char endpoint[ENDPOINT_LENGTH];
+    char data[MESSAGE_DATA_LENGTH];
+    char telegram_bot_token[BOT_TOKEN_LENGTH];
+    char chat_ids[MAX_CHAT_IDS][CHAT_ID_LENGTH];
+} coap_request_t;
 
 void coap_control_init(void);
 int coap_control(int argc, char **argv);

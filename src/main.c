630620b2d1ffc8e580e8abf339d0eef2e8da3aca
Skywalker360 <jonas.huelsmann@ive.de>
2025-02-05 20:18:10 +0100

    Implementierung CoAP- & Console-Thread.

--- src/main.c
+++ src/main.c
@@ -2,16 +2,46 @@
 // Created by vincent on 11/1/24.
 //
 
+#include <stdio.h>
+#include <ztimer.h>
+
 #include "msg.h"
 
 #include "led_control.h"
 #include "cpu_temperature.h"
 #include "cmd_control.h"
 #include "coap_post.h"
+#include "thread.h"
+#include "utils/error_handler.h"
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
+char rcv_thread_stack[THREAD_STACKSIZE_MAIN];
+
+void *coap_thread(void *arg) {
+    (void) arg;
+
+    while (true) {
+        cpu_temperature_t temp;
+        cpu_temperature_get(&temp);
+
+        coap_request_t request;
+        init_coap_request(&request);
+        const int res = coap_post_send(&request, temp.temperature);
+
+        handle_error(__func__, res);
+
+        ztimer_sleep(ZTIMER_SEC, 60);
+    }
+
+    return NULL;
+}
+
+void *console_thread(void *arg) {
+    cmd_control_init();
+    return NULL;
+}
 int main(void) {
     // Initialize devices
     led_control_init();
@@ -26,8 +56,18 @@ int main(void) {
         // call coap post method with struct
     //}
 
+    thread_create(rcv_thread_stack, sizeof(rcv_thread_stack),
+        THREAD_PRIORITY_MAIN -2, 0,
+        coap_thread, NULL, "coap_thread");
+
     //thread_2:
-    cmd_control_init();
+    #ifdef CONSOLE_USE {
+        thread_create(rcv_thread_stack, sizeof(rcv_thread_stack),
+            THREAD_PRIORITY_MAIN -1, 0,
+            console_thread, NULL, "console_thread");
+    }
+
+    #endif
 
     return 0;
 }

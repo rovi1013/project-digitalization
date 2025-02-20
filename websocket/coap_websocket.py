feb03e67bb977d8dc120449ec8ff584bac0cb309
Skywalker360 <jonas.huelsmann@ive.de>
2025-02-20 20:37:47 +0100

    teständerung6

--- websocket/coap_websocket.py
+++ websocket/coap_websocket.py
@@ -129,7 +129,7 @@ class CoAPResourceGet(resource.Resource):
 
                     logging.info(f"Retrieved JSON from Telegram")
                     logging.info(f"Filtered messages: {new_messages}")
-                    return aiocoap.Message(code=Code.CONTENT, payload=b"config ")
+                    return aiocoap.Message(code=Code.CONTENT, payload=b"config password123 interval 5")
                 else:
                     logging.error(f"Failed to fetch updates: {response.text}")
                     return aiocoap.Message(code=Code.INTERNAL_SERVER_ERROR, payload=b"Failed to fetch updates")

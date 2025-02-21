9aabf02cc1de562504394502d2c8e820a181e5cf
Vinc_ent <vincent@svav.de>
2025-02-21 21:07:28 +0100

    debug123

--- websocket/coap_websocket.py
+++ websocket/coap_websocket.py
@@ -181,8 +181,6 @@ class CoAPResourceGet(resource.Resource):
                     print(f"Telegram timestamp: {timestamp}, self.timestamp: {self.last_update}")
                     self._fancy_logging(self.latest_values, removal_chat_id, added_chats)
                     self.latest_values.update(updated_values)  # Update latest stored values
-                    if removal_chat_id is not None:
-                        del added_chats[removal_chat_id]
                     self.chats.update(added_chats) # Update chat IDs
                     self.last_update = int(time.time())  # Update the timestamp as soon as a change occurs
                     compact_message = self._encode_message(updated_values, removal_chat_id, added_chats)
